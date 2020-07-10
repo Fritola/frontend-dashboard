fef0733065f9870c549a39043a862846389148b6
Fritola <fritola23@gmail.com>
2020-07-10 10:46:15 -0300

    dev branch for beta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import LateralMenu from './Components/LateralMenu'
 import CardUser from './Components/CardUser'
-import CreateCard from './Components/CreateCard'
-
-import {ContainerCreation} from './StyledComponents/CreateCard'
 
 import api from './Services/api'
 import './App.css';
@@ -25,16 +22,13 @@ function App() {
 	},[])
 	
 	return (
-		<div className="App">
+		<div className="App">			
 			{loading != false &&
-			<>
+			<>	
 				<LateralMenu user={user}/>
 				<div className="ContentContainer">					
 					<CardUser/>
-				</div>
-				{/* <ContainerCreation>
-					<CreateCard/>
-				</ContainerCreation> */}
+				</div>				
 			</>}			
 		</div>
 	);
